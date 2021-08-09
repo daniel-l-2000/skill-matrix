5f79a7ae8224c580e88b3059b8d4eac0f293f4eb
daniel-l-2000 <87469481+daniel-l-2000@users.noreply.github.com>
2021-08-09 23:18:12 +0200

    Fix

--- src/components/layout/MainNavigation.tsx
+++ src/components/layout/MainNavigation.tsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router-dom";
+
 function MainNavigation() {
-  return <header></header>;
+  return (
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Skill Matrix
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link className="nav-link" to="/matrix">
+              Matrix
+            </Link>
+            <Link className="nav-link" to="/profile">
+              Profile
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default MainNavigation;
--- src/pages/Matrix.tsx
+++ src/pages/Matrix.tsx
@@ -1,7 +1,87 @@
-import Matrix from "../components/matrix/Matrix";
+import KnowledgeLevel from "../components/matrix/KnowledgeLevel";
+import Person from "../components/matrix/Person";
+import styles from "./Matrix.module.css";
+import Skill from "../components/matrix/Skill";
+import { useEffect, useState } from "react";
+import { firebaseGet } from "../util/firebase";
+import { Data } from "../models/data";
 
 function MatrixPage() {
-  return <Matrix></Matrix>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedData, setLoadedData] = useState<Data>();
+
+  useEffect(() => {
+    firebaseGet<Data>("/.json").then((data) => {
+      setIsLoading(false);
+      setLoadedData(data);
+      console.log(data);
+    });
+  }, []);
+
+  const skills = [
+    {
+      id: 101,
+      name: "Angular"
+    },
+    {
+      id: 102,
+      name: "React"
+    },
+    {
+      id: 103,
+      name: "Firebase"
+    }
+  ];
+
+  const people = [
+    {
+      id: 201,
+      name: "Daniel"
+    },
+    {
+      id: 202,
+      name: "Max"
+    }
+  ];
+
+  const knowledgeLevels = [
+    {
+      level: 2,
+      skillIndex: 1,
+      personIndex: 0
+    }
+  ];
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className={styles.matrixGrid}>
+      {skills.map((s, i) => (
+        <Skill name={s.name} index={i} key={s.id}></Skill>
+      ))}
+      {people.map((p, i) => (
+        <Person name={p.name} index={i} key={p.id}></Person>
+      ))}
+
+      {skills.map((s, si) => {
+        return people.map((p, pi) => {
+          const level = knowledgeLevels.find(
+            (lvl) => lvl.skillIndex === si && lvl.personIndex === pi
+          );
+          return (
+            <KnowledgeLevel
+              key={si + "_" + pi}
+              level={level?.level ?? 0}
+              skillIndex={si}
+              personIndex={pi}
+            ></KnowledgeLevel>
+          );
+        });
+      })}
+    </div>
+  );
 }
 
 export default MatrixPage;
