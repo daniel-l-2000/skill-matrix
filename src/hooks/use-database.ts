f3433ca54502852a63a85b43df628cc209a870fd
daniel-l-2000 <87469481+daniel-l-2000@users.noreply.github.com>
2021-08-19 01:34:13 +0200

    Fix infinite loop bug

--- src/hooks/use-database.ts
+++ src/hooks/use-database.ts
@@ -1,25 +1,31 @@
 import { get, getDatabase, ref, remove, set, update } from "firebase/database";
+import { useCallback } from "react";
 
 function useDatabase<T = void>(
   path: string,
   method: "create" | "read" | "update" | "delete"
 ) {
-  const startRequest = async (value?: any) => {
-    const db = getDatabase();
-    const pathRef = ref(db, path);
+  const startRequest = useCallback(
+    async (value?: any) => {
+      const db = getDatabase();
+      const pathRef = ref(db, path);
 
-    switch (method) {
-      case "create":
-        return set(pathRef, value);
-      case "read":
-        const snapshot = await get(pathRef);
-        return snapshot.val() as T | null;
-      case "update":
-        return update(pathRef, value);
-      case "delete":
-        return remove(pathRef);
-    }
-  };
+      switch (method) {
+        case "create":
+          return set(pathRef, value);
+        case "read":
+          const snapshot = await get(pathRef);
+          return snapshot.val() as T | null;
+        case "update":
+          return update(pathRef, value);
+        case "delete":
+          return remove(pathRef);
+      }
+    },
+    // because <T> parameter cannot be added to deps array
+    // eslint-disable-next-line
+    [path, method]
+  );
 
   return startRequest;
 }
