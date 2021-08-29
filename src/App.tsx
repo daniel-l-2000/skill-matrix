0ada802867b0cb7463f71040a6287aef91a9ef58
Daniel Landgraf <d.landgraf2000@icloud.com>
2021-08-29 23:38:43 +0200

    Add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,41 @@
-import { Switch, Route, Redirect } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import MatrixPage from "./components/pages/MatrixPage";
-import ProfilePage from "./components/pages/ProfilePage";
-import SignInPage from "./components/pages/SignInPage";
-import useAuth from "./hooks/use-auth";
+import React, { Suspense } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import MatrixPage from './components/pages/MatrixPage';
+import SignInPage from './components/pages/SignInPage';
+import LoadingSpinner from './components/util/LoadingSpinner';
+import useAuth from './hooks/use-auth';
+
+const ProfilePage = React.lazy(() => import('./components/pages/ProfilePage'));
 
 function App() {
   const auth = useAuth();
 
   return (
     <Layout>
-      <Switch>
-        {!auth.isSignedIn && (
-          <Route path="/" exact>
-            <SignInPage></SignInPage>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          {!auth.isSignedIn && (
+            <Route path="/" exact>
+              <SignInPage></SignInPage>
+            </Route>
+          )}
+          {auth.isSignedIn && (
+            <Route path="/matrix">
+              <MatrixPage></MatrixPage>
+            </Route>
+          )}
+          {auth.isSignedIn && (
+            <Route path="/profiles/:userId">
+              <ProfilePage></ProfilePage>
+            </Route>
+          )}
+          <Route path="*">
+            {auth.isSignedIn === false && <Redirect to="/" />}
+            {auth.isSignedIn === true && <Redirect to="/matrix" />}
           </Route>
-        )}
-        {auth.isSignedIn && (
-          <Route path="/matrix">
-            <MatrixPage></MatrixPage>
-          </Route>
-        )}
-        {auth.isSignedIn && (
-          <Route path="/profiles/:userId">
-            <ProfilePage></ProfilePage>
-          </Route>
-        )}
-        <Route path="*">
-          {auth.isSignedIn === false && <Redirect to="/" />}
-          {auth.isSignedIn === true && <Redirect to="/matrix" />}
-        </Route>
-      </Switch>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
